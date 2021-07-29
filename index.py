f333a1e7efe8b4dadcc69b56a2016a83b591616a
lhes23 <32542078+lhes23@users.noreply.github.com>
2021-07-29 17:37:13 +0800

    connect to 2 servers and add the command git pull

--- index.py
+++ index.py
@@ -1,9 +1,25 @@
 import config
 import paramiko
 
-ssh = paramiko.SSHClient()
-ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-key = paramiko.RSAKey.from_private_key_file(config.keyfile)
-ssh.connect(
-    hostname=config.host, username=config.username, look_for_keys=False, pkey=key
-)
+
+def connectSSH(host):
+    try:
+        cert = paramiko.RSAKey.from_private_key_file(config.keyfile)
+        c = paramiko.SSHClient()
+        c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        print("connecting...")
+        c.connect(hostname=host, username=config.username, pkey=cert)
+        print("connected!!!")
+        stdin, stdout, stderr = c.exec_command(
+            "cd /var/www/hosts/ippei.com/httpdocs;git pull origin live"
+        )
+        print(stdout.readlines())
+        c.close()
+
+    except:
+        print("Connection Failed!!!")
+
+
+servers = [config.host1, config.host2]
+for server in servers:
+    connectSSH(server)
