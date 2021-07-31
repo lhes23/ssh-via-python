8b23ff0fade15f25153ecf59629f9b39f58b6a5f
lhes23 <32542078+lhes23@users.noreply.github.com>
2021-07-31 19:25:24 +0800

    added an os and git commands

--- index.py
+++ index.py
@@ -1,5 +1,6 @@
 import config
 import paramiko
+import os
 
 
 def connectSSH(host):
@@ -9,16 +10,36 @@ def connectSSH(host):
         c.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         c.connect(hostname=host, username=config.username, pkey=cert)
         print("connected!!!")
-        stdin, stdout, stderr = c.exec_command(
-            "cd /var/www/hosts/ippei.com/httpdocs;git pull origin live"
-        )
+        stdin, stdout, stderr = c.exec_command("cd /var/www/hosts/ippei.com/httpdocs;git pull origin live")
         print(stdout.readlines())
         c.close()
         print("Successfully Updated")
     except:
         print("Connection Failed!!!")
 
+def updateAwsServers():
+    servers = [config.host1, config.host2]
+    for server in servers:
+        connectSSH(server)
 
-servers = [config.host1, config.host2]
-for server in servers:
-    connectSSH(server)
+def installUpdate(loc,item):
+    filename = input("Name of the file?")
+    base_dir = "~/Documents/Github/ippei"
+    file = filename.split(".zip")
+    msg = "Install/Update " + item + " - " + file[0]
+    
+    os.system("cd " + base_dir + ";git pull;cp ~/downloads/"+ filename + " " + base_dir + "" + loc + "/" + filename + ";cd " + base_dir + "" + loc + "/;unzip -o " + filename + ";rm " + filename + ";cd " + base_dir + ":git commit -m '" + msg + "';git push origin live")
+    
+
+process = input("[0]Plugin or [1]Theme?")
+
+if process == "0":
+    loc = "/wp-content/plugins"
+    installUpdate(loc, "plugin")
+    # updateAwsServers()
+elif process == "1":
+    loc = "/wp-content/themes"
+    installUpdate(loc, "theme")
+    # updateAwsServers()
+else:
+    print("Wrong Answer!")
